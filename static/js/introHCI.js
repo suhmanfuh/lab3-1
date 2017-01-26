5ab8fa3fcfbd9e152c9f2c0fef86a83d991b862d
Samantha Antonious <saantoni@ucsd.edu>
2017-01-26 11:10:58 -0800

    finished lab three !

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,26 +15,35 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-	$("testjs").click(function(e) {
+
  	  $(".jumbotron h1").text("Javascript has taken over!!!");
-		$("#testjs").text("Please wait...");
+		$(this).text("Please wait...");
 
         $(".jumbotron p").addClass("active");
-	});
+	}
 }
-
-function projectClick(e) {
+    $(".project").click(function(e){
     // prevent the page from reloading¿     
     e.preventDefault();
-    // In an event handler, $(this) refers to¿     
-    // the object that triggered the event¿     
-    //$(this).css("background-color", "#7fff00");
+
+    // In an event handler, $(this) refers to     
+    // the object that triggered the event   
+    
     var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       $(description).fadeOut();
+           description = $(containingProject).find(".project-description:hidden");
+            console.log (description);
+            if (description.length === 0) {
+                description = $(containingProject).find(".project-description");
+                $(description).fadeOut();
+            }
+            else {
+                $(description).fadeIn();
+            }
+       
        //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
     
     }
